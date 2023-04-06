18b330ca2286cd34392352cd4da68b77b4cd0d91
Kelvison Carlos Vieira <kelvison.vieira@live.com>
2023-04-05 23:26:13 -0300

    Adicionando arquivos iniciais

---
+++ index.js
@@ -0,0 +1,22 @@
+const http= require('http');
+const fs = require('fs');
+const port = 3000
+
+const server = http.createServer((req,res)=>{
+ fs.readFile('./arquivo.html',(err, data)=>{
+    if(err){
+        res.writeHead(404);
+        res.write('arquivo não encontrado')
+     }
+    else{
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data);
+     }
+    return res.end();    
+ })
+})
+
+server.listen(port,()=>{
+    console.log("servidor rodando na porta  "+port);
+})
+
